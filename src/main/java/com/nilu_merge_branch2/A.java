5c58c16128b62303b0b52f5b84e463389b012114
Niladri314 <126091593+Niladri314@users.noreply.github.com>
2025-03-21 09:25:32 +0530

    Update A.java

--- src/main/java/com/nilu_merge_branch2/A.java
+++ src/main/java/com/nilu_merge_branch2/A.java
@@ -3,10 +3,7 @@ package com.nilu_merge_branch2;
 public class A {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-<<<<<<< HEAD
-        System.out.println("one tea curi");
-=======
         System.out.println("added another code line");
->>>>>>> 399103624ae52494af6f97b5f1f9c133ea949a19
+          System.out.println("one tea curi");
     }
 }
